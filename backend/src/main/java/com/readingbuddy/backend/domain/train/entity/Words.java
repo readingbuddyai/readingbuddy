6485df714c5e7835382243248ff308c33e9166d7
백태균 <taegyunb99@gmail.com>
2025-10-28 15:43:42 +0900

    WIP: 1.2단계 개발 중

--- backend/src/main/java/com/readingbuddy/backend/domain/train/entity/Words.java
+++ backend/src/main/java/com/readingbuddy/backend/domain/train/entity/Words.java
@@ -1,11 +1,23 @@
 package com.readingbuddy.backend.domain.train.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
+@Table(name = "words")
 public class Words {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
+    private String word;
+
+    @Column(name = "voice_url")
+    private String voiceUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "phoneme_id")
+    private Phonemes phoneme;
 }
