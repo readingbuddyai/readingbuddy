13db102a6a63eb6c3ff473577bbc501338ff74a1
cheesecrust <jack0810@naver.com>
2025-11-03 09:10:50 +0900

    Feature: add backend/.

--- backend/src/main/java/com/readingbuddy/backend/domain/dashboard/controller/DashBoardController.java
+++ backend/src/main/java/com/readingbuddy/backend/domain/dashboard/controller/DashBoardController.java
@@ -1,8 +1,13 @@
 package com.readingbuddy.backend.domain.dashboard.controller;
 
+import com.readingbuddy.backend.common.util.format.ApiResponse;
 import com.readingbuddy.backend.domain.dashboard.service.DashBoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashBoardController {
 
     private final DashBoardService dashBoardService;
+
+    @GetMapping(value = "/stage/info")
+    public ResponseEntity<ApiResponse<?>> stageInfo(@RequestParam String stage) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("a"));
+    }
 }
